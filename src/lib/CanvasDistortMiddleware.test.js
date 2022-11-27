5819a3fa961187334b745c017d2e12343f8e25cf
Ryan Berliner <ryan.berliner@gmail.com>
2022-11-27 10:47:30 -0500

    tests canvas distory middleware

---
+++ src/lib/CanvasDistortMiddleware.test.js
@@ -0,0 +1,42 @@
+import {
+  setImgUrl,
+  setImgDetails,
+  updateStageZoom,
+  updateStagePosition,
+} from '../workbenchReducer';
+
+import { CanvasDistort } from './CanvasDistort';
+import { WorkbenchMiddleware } from './CanvasDistortMiddleware';
+
+describe('canvas distort middleware', () => {
+  let canvasDistort;
+
+  beforeEach(() => {
+    canvasDistort = new CanvasDistort();
+  })
+
+  it('sets image url', () => {
+    const middleware = WorkbenchMiddleware(canvasDistort).fn;
+    middleware([setImgUrl('lorem')]);
+    expect(canvasDistort.imageUrl).toBe('lorem');;
+  })
+
+  it('sets image details', () => {
+    const middleware = WorkbenchMiddleware(canvasDistort).fn;
+    middleware([setImgDetails('lorem')]);
+    expect(canvasDistort.imageDetails).toBe('lorem');;
+  })
+
+  it('updates zoom', () => {
+    const middleware = WorkbenchMiddleware(canvasDistort).fn;
+    middleware([updateStageZoom(90)]);
+    expect(canvasDistort.zoom).toBe(90);;
+  })
+
+  it('updates stage position', () => {
+    const middleware = WorkbenchMiddleware(canvasDistort).fn;
+    middleware([updateStagePosition(1, 2)]);
+    expect(canvasDistort.posX).toBe(1);;
+    expect(canvasDistort.posY).toBe(2);;
+  })
+});
